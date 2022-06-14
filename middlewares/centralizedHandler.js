a87522ab258873b02f54b13096339e6c6f5ce617
Iliya Gomon <iliya.gomon@msapps.mobi>
2022-06-14 14:22:53 +0300

    centrlized error handler corrected

--- middlewares/centralizedHandler.js
+++ middlewares/centralizedHandler.js
@@ -1,10 +1,6 @@
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? `Error ${statusCode} - An error occurred on the server`
-        : `Error ${statusCode} - ${message}`,
-    });
+  res.status(statusCode).send({
+    message: `Error ${statusCode} - ${message}`,
+  });
 };
